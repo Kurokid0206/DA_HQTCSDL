eed5d91957eb6a0e00fcd83940193e740f844753
Kurokid0206 <hungtyvuive@gmail.com>
2021-12-16 17:52:38 +0700

    connect mssql

--- index.js
+++ index.js
@@ -16,3 +16,19 @@ const sql = require('mssql');
 app.listen(3000, function(){
 	console.log("server is listen on port 3000.");
 });
+
+config = {
+    user: 'sa',
+    password: '.',
+    server: 'localhost',
+    database: 'qlGH',
+    port: 1433,
+    options: {
+        trustServerCertificate: true // change to true for local dev / self-signed certs
+    }
+}
+
+app.get("/",function(req,res){
+
+    res.sendFile(__dirname+"/index.html")
+})
