5aa6679fe013253c7710788ad518cc24bcdb5cfa
Mean-Sun <tranminhsonyi@gmail.com>
2021-12-21 15:48:55 +0700

    update registration

---
+++ publics/js/homepage.js
@@ -0,0 +1,17 @@
+function show_customer_form() {
+    document.getElementById("customer-form").style.display = "block";
+    document.getElementById("partner-form").style.display = "none";
+    document.getElementById("driver-form").style.display = "none";
+}
+
+function show_partner_form() {
+    document.getElementById("customer-form").style.display = "none";
+    document.getElementById("partner-form").style.display = "block";
+    document.getElementById("driver-form").style.display = "none";
+}
+
+function show_driver_form() {
+    document.getElementById("customer-form").style.display = "none";
+    document.getElementById("partner-form").style.display = "none";
+    document.getElementById("driver-form").style.display = "block";
+}
